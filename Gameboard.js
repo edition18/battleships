7594c052404ecd210866336abec7da1927e68203
Edwin Choo <jchoo004@e.ntu.edu.sg>
2021-01-02 01:57:18 +0800

    second commit

--- Gameboard.js
+++ Gameboard.js
@@ -23,11 +23,6 @@ export function Gameboard() {
   let ships = [];
 
   const getBoard = () => board;
-  const shipOrientation = () => {
-    const orientation = ["l", "r", "t", "d"];
-    return orientation[Math.floor(Math.random() * orientation.length)];
-  };
-
   const random0to9 = () => {
     return Math.floor(Math.random() * 10);
   };
@@ -36,33 +31,28 @@ export function Gameboard() {
     return [random0to9(), random0to9()];
   };
 
-  const generatePositionsNeeded = (length, startingPosition, orientation) => {
+  const generatePositionsNeeded = (length, startingPosition, vertical) => {
     // startingPosition = [a,b] , i.e. row then column
     const positionsNeeded = [];
-    switch (orientation) {
-      case "d":
-        for (let i = 0; i < length; i++) {
-          positionsNeeded.push([startingPosition[0] - i, startingPosition[1]]);
-        }
-        return positionsNeeded;
-      case "t":
-        for (let i = 0; i < length; i++) {
-          positionsNeeded.push([startingPosition[0] + i, startingPosition[1]]);
-        }
-        return positionsNeeded;
-      case "r":
+    switch (vertical) {
+      case false: // horizontal
         for (let i = 0; i < length; i++) {
           positionsNeeded.push([startingPosition[0], startingPosition[1] + i]);
         }
         return positionsNeeded;
-      case "l":
+      case true: //vertical
         for (let i = 0; i < length; i++) {
-          positionsNeeded.push([startingPosition[0], startingPosition[1] - i]);
+          positionsNeeded.push([startingPosition[0] + i, startingPosition[1]]);
         }
         return positionsNeeded;
       default:
         return;
     }
+  };
+
+  const randomTrueOrFalse = () => {
+    const array = [1, 2];
+    return array.sample == 1 ? true : false;
   };
 
   const populateShipsAtRandom = () => {
@@ -72,89 +62,61 @@ export function Gameboard() {
         name: "carrier",
         length: 5,
         startingPosition: randomXYPosition(),
-        orientation: shipOrientation(),
+        vertical: randomTrueOrFalse(),
       },
       {
         name: "battleship",
         length: 4,
         startingPosition: randomXYPosition(),
-        orientation: shipOrientation(),
+        vertical: randomTrueOrFalse(),
       },
       {
         name: "cruiser",
         length: 3,
         startingPosition: randomXYPosition(),
-        orientation: shipOrientation(),
+        vertical: randomTrueOrFalse(),
       },
       {
         name: "submarine",
         length: 3,
         startingPosition: randomXYPosition(),
-        orientation: shipOrientation(),
+        vertical: randomTrueOrFalse(),
       },
       {
         name: "destroyer",
         length: 2,
         startingPosition: randomXYPosition(),
-        orientation: shipOrientation(),
+        vertical: randomTrueOrFalse(),
       },
     ];
 
+    // startingShipSpecifications.forEach((ship) => {
+    //   // const positionsNeeded = generatePositionsNeeded(
+    //   //   ship.length,
+    //   //   ship.startingPosition,
+    //   //   ship.vertical
+    //   // );
+    //   // positionsNeeded.map((position) => test.push(position));
+    // });
+    test = [];
     startingShipSpecifications.forEach((ship) => {
       const addToGameboard = () => {
         const positionsNeeded = generatePositionsNeeded(
           ship.length,
           ship.startingPosition,
-          ship.orientation
+          ship.vertical
         );
 
         if (positionAvailable(positionsNeeded)) {
           addShip(ship, positionsNeeded);
+          test.push(positionsNeeded);
         } else {
-          ship.orientation = shipOrientation();
           ship.startingPosition = randomXYPosition();
-          addToGameboard();
         }
       };
       addToGameboard();
     });
-    return ships;
-    // addShip(
-    //   {
-    //     name: "carrier",
-    //     length: 5,
-    //     startingPosition: [1, 5],
-    //     orientation: "t",
-    //   },
-    //   [
-    //     [1, 5],
-    //     [2, 5],
-    //     [3, 5],
-    //     [4, 5],
-    //     [5, 5],
-    //   ]
-    // );
-    // return ships[0].getAllPositions();
-  };
-
-  const checkAndAddToBoard = (ship) => {
-    const positionsNeeded = generatePositionsNeeded(
-      ship.length,
-      ship.startingPosition,
-      ship.orientation
-    );
-    if (positionAvailable(positionsNeeded)) {
-      addShip(ship, positionsNeeded);
-    } else {
-      let randomOrientation = shipOrientation();
-      let randomStartingPosition = randomXYPosition();
-      const newShip = {
-        ...ship,
-        startingPosition: randomStartingPosition,
-        orientation: randomOrientation,
-      };
-      checkAndAddToBoard(newShip);
-    }
+    return test;
   };
 
   const addShip = (ship, positionsNeeded) => {
@@ -166,7 +128,7 @@ export function Gameboard() {
     ships.forEach((ship) => {
       let positions = ship.getAllPositions();
       positions.forEach(
-        (position) => (board[(position[0], position[1])] = "ship")
+        (position) => (board[position[0]][position[1]] = "ship")
       );
     });
   };
@@ -187,7 +149,6 @@ export function Gameboard() {
 
   return {
     getBoard,
-    shipOrientation,
     random0to9,
     randomXYPosition,
     generatePositionsNeeded,
--- index.test.js
+++ index.test.js
@@ -2,90 +2,126 @@ const { sum } = require("./index.js");
 const { Gameboard } = require("./Gameboard.js");
 import { Ship } from "./Ship.js";
 
-test("board returns empty state with 100 possible grids", () => {
-  expect(Gameboard().getBoard()).toStrictEqual([
-    ["", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", ""],
-  ]);
-});
+describe("Gameboard factory", () => {
+  it("board returns empty state with 100 possible grids", () => {
+    expect(Gameboard().getBoard()).toStrictEqual([
+      ["", "", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", "", "", ""],
+    ]);
+  });
 
-expect.extend({
-  rangeOfOrientation(received) {
-    const array = ["l", "r", "t", "d"]; //possible answers
-    if (array.includes(received))
-      return {
-        pass: true,
-      };
-    else {
-      return {
-        pass: false,
-      };
-    }
-  },
-  rangeOfIntegers(received) {
-    const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; //possible answers
-    if (array.includes(received))
-      return {
-        pass: true,
-      };
-    else {
-      return {
-        pass: false,
-      };
-    }
-  },
-  randomXYPosition(received) {
-    if (
-      received[0] < 10 &&
-      received[1] < 10 &&
-      Number(received[0]) &&
-      Number(received[1])
-    )
-      return {
-        pass: true,
-      };
-    else {
-      return {
-        pass: false,
-      };
-    }
-  },
-});
+  expect.extend({
+    rangeOfBooleans(received) {
+      const array = [true, false]; //possible answers
+      if (array.includes(received))
+        return {
+          pass: true,
+        };
+      else {
+        return {
+          pass: false,
+        };
+      }
+    },
+    rangeOfIntegers(received) {
+      const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; //possible answers
+      if (array.includes(received))
+        return {
+          pass: true,
+        };
+      else {
+        return {
+          pass: false,
+        };
+      }
+    },
+    randomXYPosition(received) {
+      if (
+        received[0] < 10 &&
+        received[1] < 10 &&
+        Number(received[0]) &&
+        Number(received[1])
+      )
+        return {
+          pass: true,
+        };
+      else {
+        return {
+          pass: false,
+        };
+      }
+    },
+    countNumberOfShips(received) {
+      let count = 0;
+      received.forEach((row) =>
+        row.forEach((column) => (column == "ship" ? (count = count + 1) : ""))
+      );
+      if (count == 17)
+        return {
+          pass: true,
+          message: "clear",
+        };
+      else {
+        return {
+          pass: false,
+          message: count,
+        };
+      }
+    },
+    countNumberOfPositions(received) {
+      if (received.length == 17)
+        return {
+          pass: true,
+        };
+      else {
+        return {
+          pass: false,
+          message: received.length,
+        };
+      }
+    },
+  });
 
-test("ships orientation left right top or down", () => {
-  expect(Gameboard().shipOrientation()).rangeOfOrientation();
-});
+  it("position value generator btwn 0-9", () => {
+    expect(Gameboard().random0to9()).rangeOfIntegers();
+  });
 
-test("position value generator btwn 0-9", () => {
-  expect(Gameboard().random0to9()).rangeOfIntegers();
-});
+  it("random XY position", () => {
+    expect(Gameboard().randomXYPosition());
+  });
 
-test("random XY position", () => {
-  expect(Gameboard().randomXYPosition());
-});
+  it("check generated position needed => vertical", () => {
+    expect(Gameboard().generatePositionsNeeded(2, [1, 2], true)).toStrictEqual([
+      [1, 2],
+      [2, 2],
+    ]);
+  });
 
-test("check generated position needed", () => {
-  expect(Gameboard().generatePositionsNeeded(2, [1, 2], "l")).toStrictEqual([
-    [1, 2],
-    [1, 1],
-  ]);
-});
+  it("check generated position needed => horizontal", () => {
+    expect(Gameboard().generatePositionsNeeded(3, [1, 2], false)).toStrictEqual(
+      [
+        [1, 2],
+        [1, 3],
+        [1, 4],
+      ]
+    );
+  });
 
-test("position available", () => {
-  expect(Gameboard().positionAvailable([1, 2])).toBe(true);
-});
+  it("position available", () => {
+    expect(Gameboard().positionAvailable([1, 2])).toBe(true);
+  });
 
-test("position avaiable", () => {
-  expect(Gameboard().populateShipsAtRandom()).toBe(true);
+  it("position avaiable", () => {
+    const gb = Gameboard();
+    gb.populateShipsAtRandom();
+    expect(gb.getBoard()).countNumberOfShips();
+  });
 });
-// test("ships array", () => {
-//   expect(Gameboard().shipOrientation()).toBe("l" || "r" || "t" || "d");
-// });
